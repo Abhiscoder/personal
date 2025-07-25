19af9cac411c599f4f7c652bfa5f3cf2efc998c2
Abhishek Kumar <115029976+Abhiscoder@users.noreply.github.com>
2025-07-25 12:52:24 +0530

    Update About.jsx
    update resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1x2ouJptAxIZO1gO_CpKW2Dv8UJtPm4Un/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1EtJT4RkK6tM2S6LBd05xrND3renlyXrI/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
